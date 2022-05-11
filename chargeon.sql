24b4eed1ce3ea9e8313491f78783d23f81324ce7
DAVID Louis <92583575+lodavid38600@users.noreply.github.com>
2022-05-11 08:22:05 +0200

    Base de données SQL

---
+++ chargeon.sql
@@ -0,0 +1,91 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : mer. 11 mai 2022 à 06:21
+-- Version du serveur :  8.0.21
+-- Version de PHP : 7.3.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `chargeon`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `chargeur`
+--
+
+DROP TABLE IF EXISTS `chargeur`;
+CREATE TABLE IF NOT EXISTS `chargeur` (
+  `num_serie` int NOT NULL,
+  `type` varchar(255) NOT NULL COMMENT 'Extérieur / intérieur',
+  `protection` varchar(255) NOT NULL COMMENT 'IP 44 ou 55 etc...',
+  `puissance` varchar(255) NOT NULL COMMENT 'KW',
+  `priorité` int NOT NULL COMMENT 'Haute prio = 1\r\nMoyenne = 2\r\nBasse = 3',
+  `lattitude` varchar(255) NOT NULL,
+  `longitude` varchar(255) NOT NULL,
+  `altitude` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `cout`
+--
+
+DROP TABLE IF EXISTS `cout`;
+CREATE TABLE IF NOT EXISTS `cout` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `montant` varchar(255) NOT NULL,
+  `support` varchar(255) DEFAULT NULL,
+  `zone` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `intervention`
+--
+
+DROP TABLE IF EXISTS `intervention`;
+CREATE TABLE IF NOT EXISTS `intervention` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_cout` int NOT NULL,
+  `id_chargeur` int NOT NULL,
+  `mission` int NOT NULL COMMENT 'Installation 1/entretien 2 / Panne 3',
+  `temps` varchar(255) NOT NULL,
+  `concurrence` int NOT NULL COMMENT '1 = concurrence proche\r\n0 = pas de concurrence',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `technicien`
+--
+
+DROP TABLE IF EXISTS `technicien`;
+CREATE TABLE IF NOT EXISTS `technicien` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_intervention` int NOT NULL,
+  `niveau` varchar(255) NOT NULL COMMENT 'B1/B2 etc...',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
